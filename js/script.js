4afea17ef1eab63182ed5d0a1a19b5c54473a8a8
FrancescaLai <francescalai1690@gmail.com>
2021-03-17 14:32:25 +0100

    Es 2 done ✓

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-// Creare un array di oggetti di studenti.
-// Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
 // Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 
 
@@ -14,4 +12,30 @@ var studente = {
 
 for (var i in studente) {
   console.log(i + " " + studente[i]);
+}
+
+
+/* 2. Creare un array di oggetti di studenti.
+Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.*/
+
+var arrayStudenti = [
+  {
+    nome : "francesca",
+    cognome : "lai",
+    eta : 30
+  },
+  {
+    nome : "martina",
+    cognome : "bianchi",
+    eta : 27
+  },
+  {
+    nome : "federica",
+    cognome : "rossi",
+    eta : 32
+  },
+];
+
+for (var i = 0; i < arrayStudenti.length; i++) {
+  console.log(arrayStudenti[i].nome + " " + arrayStudenti[i].cognome + " " + arrayStudenti[i].eta);
 }
